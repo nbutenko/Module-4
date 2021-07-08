bfc7957973133ff724acec3a7da0f50fe3dbfb7c
Nadea Butenko <nadea.butenko@gmail.com>
2021-07-07 21:51:23 -0500

    Update db_module.sql

--- db_module.sql
+++ db_module.sql
@@ -1,15 +1,3 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               10.1.10-MariaDB - mariadb.org binary distribution
--- ОС Сервера:                   Win32
--- HeidiSQL Версия:              9.3.0.4984
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 -- Дамп структуры базы данных Module4
 CREATE DATABASE IF NOT EXISTS `module4` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `Module4`;
@@ -151,7 +139,3 @@ INSERT INTO `users` (`id`, `login`, `email`, `salt`, `password`, `is_admin`) VAL
 	(0000000032, 'opasia', 'o@p', 7225646, 'a64fffcccd204a316ef8659e274c451a', 0),
 	(0000000033, 'a', 'a@a', 495910, 'a437be4cc1d0a33b9ea5af9cf009c070', 0),
 	(0000000034, 'b', 'b@b', 4609069, '3ca791c3868ae5966e7f04c4cee5f02a', 0);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
